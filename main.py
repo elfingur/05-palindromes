3c140fa1598cbb0571fb0e2bcd437f03b504ec96
Léo-LONG <leo.long@edu.esiee.fr>
2025-11-13 09:44:24 +0000

    Version 2

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+"""Le programme à pour but principal ispalindrome() qui renvoie la valeur de la question
+    La chaine passer een paramètre est elle un palindrome ?
+    """
 #### Fonction secondaire
 
 
 def ispalindrome(p):
+    """fonction : teste si la chaine passer en paramètre est un palindrome
+        Paramètre : 
+            p : la chaine tester
+        Return : 
+            true : si la chaine est un palindrome
+            false : si elle ne l'est pas
+        """
     table = str.maketrans({
-    # Lettres minuscules
     'à': 'a', 'â': 'a', 'ä': 'a',
     'á': 'a', 'ã': 'a', 'å': 'a',
     'ç': 'c',
@@ -12,7 +21,9 @@ def ispalindrome(p):
     'ñ': 'n',
     'ó': 'o', 'ò': 'o', 'ô': 'o', 'ö': 'o', 'õ': 'o',
     'ú': 'u', 'ù': 'u', 'û': 'u', 'ü': 'u',
-    'ý': 'y', 'ÿ': 'y',
+    'ý': 'y', 'ÿ': 'y', ' ' : None, ',' : None, ';' : None
+    , '!' : None, '.' : None, '?' : None, '"' : None, '-' : None, '\'' : None
+    , ':' : None
     })
     p = str(p).lower().translate(table)
     return p == p[::-1]
@@ -21,9 +32,9 @@ def ispalindrome(p):
 
 
 def main():
-
+    """main ddu programme : teste l'appel de ispalindrome()"""
     # vos appels à la fonction secondaire ici
-    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie", "Épée"]:
+    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie", "é pé"]:
         print(s, ispalindrome(s))
 
 
